57bb19d68d7554c66cb8d08ae171f94cf90f8f5f
Optimus-alfaomega <a-and-o-technologies@outlook.com>
2022-05-24 12:37:16 -0500

    first commit

---
+++ src/Operator.java
@@ -0,0 +1,4 @@
+
+public enum Operator {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, BLANK
+}
---
+++ src/search/Search3.java
@@ -0,0 +1,6 @@
+package search;
+//Java-Casos-Practicos/Ch 17. Hard/Q17_17_Multi_Search
+
+public class Search3 {
+
+}
